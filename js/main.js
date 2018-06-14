995c3a4548d4b0286c1a2fe47acbbe75ff02527a
Tris <tris magp>
2018-06-14 02:24:34 +0200

    search enabled

--- js/main.js
+++ js/main.js
@@ -1,51 +1,84 @@
 const nbStudentsPerPage = 10;
-const nbStudents = $("li.student-item").length;
 
-// calculating the number of pages needed
-// depending on the total number of students and
-// and number of students we want to display per page
-const nbPages = Math.ceil(nbStudents / nbStudentsPerPage);
+function displayStudents(){
+  const nbStudents = $("li.student-item.show").length;
+  console.log(nbStudents);
+  // calculating the number of pages needed
+  // depending on the total number of students and
+  // and number of students we want to display per page
+  const nbPages = Math.ceil(nbStudents / nbStudentsPerPage);
+  $('ul.pagination').remove();
+  if(nbPages > 1){
+    // appending the unorder list for the pagination links
+    $('.page').append('<ul class="pagination"></ul>')
 
-if(nbPages > 1){
-  // appending the unorder list for the pagination links
-  $('.page').append('<ul class="pagination"></ul>')
+    // appending the pagination links
+    for (var i = 0; i < nbPages; i++) {
+      $('ul.pagination').append(`<li><a>${i+1}</a></li>`)
+    }
 
-  // appending the pagination links
-  for (var i = 0; i < nbPages; i++) {
-    $('ul.pagination').append(`<li><a>${i+1}</a></li>`)
+    //adding the class "active" to the first pagination link
+    // $('ul.pagination').find('a').eq(0).addClass('active');
+    $('ul.pagination a').eq(0).addClass('active');
   }
 
-  //adding the class "active" to the first pagination link
-  $('ul.pagination').find('a').eq(0).addClass('active');
+  applyPaginationLinks();
 
   // hiding all the students
-  $("li.student-item").hide();
+  $("li.student-item.hide").hide();
   // ..then displaying only the first page students
   for(var i = 0; i < nbStudentsPerPage; i ++){
-    $("li.student-item").eq(i).show();
+    $("li.student-item.show").eq(i).show();
   }
 }
 
+function applyPaginationLinks(){
+  // pagination links click listener
+  $('ul.pagination a').on('click',function(){
 
-// pagination links click listener
-$('a').on('click',function(){
+    // calculating the students maxIndex and minIndex
+    // than we want to display in one page
+    const pageNum = $(this).html();
+    const maxIndex = nbStudentsPerPage * pageNum;
+    const minIndex = maxIndex - nbStudentsPerPage;
 
-  // calculating the students maxIndex and minIndex
-  // than we want to display in one page
-  const pageNum = $(this).html();
-  const maxIndex = nbStudentsPerPage * pageNum;
-  const minIndex = maxIndex - nbStudentsPerPage;
+    // hiding all the students first
+    $("li.student-item").hide();
+    console.log($("li.student-item").length);
 
-  // hiding all the students first
-  $("li.student-item").hide();
+    // displaying the students from minIndex to maxIndex
+    for(var i = minIndex; i < maxIndex; i ++){
+      $("li.student-item.show").eq(i).show();
+    }
 
-  // displaying the students from minIndex to maxIndex
-  for(var i = minIndex; i < maxIndex; i ++){
-    $("li.student-item").eq(i).show();
-  }
+    // removing the class 'active' from all the pagination links
+    $('ul.pagination a').removeClass('active');
+    // adding the class 'active' to the pagination link that has just been clicked
+    $(this).addClass('active');
+  })
+}
 
-  // removing the class 'active' from all the pagination links
-  $('a').removeClass('active');
-  // adding the class 'active' to the pagination link that has just been clicked
-  $(this).addClass('active');
-})
+function addSearch(){
+  $('.page-header').append(`
+    <div class="search">
+      <input type="text" placeholder="Search for students"/> <button>Search</button>
+    </div>
+  `);
+  $('.search button').on('click', function(){
+    $('.student-item').each(function(){
+      if($(this).find('h3').text().search($('.search input').val()) > -1){
+        $(this).removeClass('hide');
+        $(this).addClass('show');
+      }else{
+        $(this).removeClass('show');
+        $(this).addClass('hide');
+      }
+    });
+    displayStudents();
+  })
+}
+
+
+$("li.student-item").addClass("show");
+displayStudents();
+addSearch();
